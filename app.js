5300a36b9ba3494af7fa3441815ec2413195072b
Corey Caire <cryancaire@gmail.com>
2022-01-21 08:24:55 -0600

    Updated to allow configurable api key

--- app.js
+++ app.js
@@ -7,13 +7,38 @@ const app = express();
 const port = process.env.PORT || 4567;
 
 
-app.get('/translate/:toLang/:toTranslate', async (req, res) => {
+app.get('/translate/:api_key/:toLang/:toTranslate', async (req, res) => {
+    if (!req.params.api_key) {
+        res.send("Error, no API Key provided!");
+    }
+
     let lang = req.params.toLang || 'en';
-
     let splitText = req.params.toTranslate.split(" ");
-    
     let removedLang = splitText.shift();
+    splitText = splitText.join(" ");
 
+    var options = {
+        method: 'POST',
+        url: 'https://microsoft-translator-text.p.rapidapi.com/translate',
+        params: {to: lang, 'api-version': '3.0', profanityAction: 'NoAction', textType: 'plain'},
+        headers: {
+          'content-type': 'application/json',
+          'x-rapidapi-host': 'microsoft-translator-text.p.rapidapi.com',
+          'x-rapidapi-key': req.params.api_key
+        },
+        data: [{Text: splitText }]
+      };
+    axios.request(options).then(function (response) {
+        res.send(response.data[0].translations[0].text);
+    }).catch(function (error) {
+        console.error(error);
+    });
+});
+
+/* app.get('/translate/:toLang/:toTranslate', async (req, res) => {
+    let lang = req.params.toLang || 'en';
+    let splitText = req.params.toTranslate.split(" ");
+    let removedLang = splitText.shift();
     splitText = splitText.join(" ");
 
     var options = {
@@ -33,7 +58,7 @@ app.get('/translate/:toLang/:toTranslate', async (req, res) => {
         console.error(error);
     });
 });
-
+ */
 
 const server = app.listen(port, () => {
     console.log(`Server is listening on ${port}`);
