901d9d54b600d980d89490a9813bae22d537bf0a
Corey Caire <cryancaire@gmail.com>
2022-01-27 10:05:52 -0600

    Update app.js

--- app.js
+++ app.js
@@ -8,8 +8,7 @@ const port = process.env.PORT || 4567;
 
 
 app.get('/translate/:api_key/:toLang/:toTranslate', async (req, res) => {
-    let channelName = req.headers['nightbot-channel'].split("name=")[0] || undefined;
-    console.log(req.headers['nightbot-channel'].split("name="))
+    let channelName = req.headers['nightbot-channel'].split("name=")[1].split("&")[0] || undefined;
     if (channelName) {
         console.log(`Channel Name = ${channelName}`)
     }
